7ee09d5ed7afd6e246b851f02496d0214f7d01b8
池樱千幻 <1299228270@qq.com>
2024-03-19 23:56:24 +0800

    添加菜单

---
+++ src/modules/onmyoji/views/base_shishen.vue
@@ -0,0 +1,12 @@
+<!--
+ * @Description: 基础数据管理-式神管理
+ * @Autor: 池樱千幻
+ * @Change: 池樱千幻
+ * @Date: 2024-03-19 23:48:28
+ * @LastEditTime: 2024-03-19 23:53:52
+-->
+<template>
+  <div>基础数据管理-式神管理</div>
+</template>
+
+<script lang="ts" name="onmyoji_base_shishen" setup></script>
---
+++ src/modules/onmyoji/views/user.vue
@@ -0,0 +1,12 @@
+<!--
+ * @Description: 阴阳师账号管理
+ * @Autor: 池樱千幻
+ * @Change: 池樱千幻
+ * @Date: 2024-03-19 23:31:58
+ * @LastEditTime: 2024-03-19 23:52:20
+-->
+<template>
+  <div>阴阳师账号管理</div>
+</template>
+
+<script lang="ts" name="onmyoji_user" setup></script>
