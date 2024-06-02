be028d7da7af2ce2932b1719973385bbce256158
池樱千幻 <1299228270@qq.com>
2024-06-02 17:15:36 +0800

    socketIo联调

--- src/modules/magicTools/views/uploadLargeFilesInPieces.vue
+++ src/modules/magicTools/views/uploadLargeFilesInPieces.vue
@@ -1,11 +1,68 @@
 <template>
-	<div><cl-upload v-model="urls" /></div>
+	<div>
+		<h2>{{ id }}</h2>
+		<div>
+			<div v-for="(item, index) in msgList" :key="index">
+				<span>{{ item.id }} 说: </span>
+				<span>{{ item.msg }}</span>
+			</div>
+		</div>
+		<div>
+			<el-form @submit="send">
+				<el-form-item>
+					<el-input v-model="msg" />
+					<el-button @click="send"> 发送</el-button>
+				</el-form-item>
+			</el-form>
+		</div>
+	</div>
 </template>
 
 <script lang="ts" name="magicTools-uploadLargeFilesInPieces" setup>
 import { useCrud, useTable, useUpsert } from "@cool-vue/crud";
 import { useCool } from "/@/cool";
+import { ElMessage, ElMessageBox } from "element-plus";
+
 import { ref } from "vue";
+import io from "socket.io-client";
+import { uuid } from "/@/cool/utils";
+import { useBase } from "/$/base";
+const { user } = useBase();
+
+let msgList = ref<any[]>([]);
+let msg = ref<string>("");
+let id = ref<string>("");
+
+console.log("user.info: ", user.info);
+
+const socket = io(
+	`http://localhost:7001/chat?userId=${user.info?.id}&type=${user.info?.id === 1 ? "service" : "user"}`
+);
+
+socket.on("data", (msg) => {
+	console.log("服务端消息", msg);
+	switch (msg.type) {
+		case "connect":
+			id.value = msg.data.id;
+			msgList.value = msg.data.hisMsgList;
+
+			ElMessage.success("连接成功");
+			break;
+		case "message":
+			msgList.value.push(msg.data);
+			break;
+		default:
+			break;
+	}
+});
+
+function send(e) {
+	socket.emit("sendMsg", { id: id.value, msg: msg.value });
+	msgList.value.push({ id: "我", msg: msg.value });
+	msg.value = "";
+	e.preventDefault();
+	return false;
+}
 
 const { service } = useCool();
 
