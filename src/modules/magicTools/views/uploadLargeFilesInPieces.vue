67ce19f0477f5221d4c87c47f739904fb696dafa
池樱千幻 <1299228270@qq.com>
2024-06-03 18:08:53 +0800

    客服聊天

--- src/modules/magicTools/views/uploadLargeFilesInPieces.vue
+++ src/modules/magicTools/views/uploadLargeFilesInPieces.vue
@@ -1,6 +1,17 @@
 <template>
 	<div>
-		<h2>{{ id }}</h2>
+		<div>
+			<div v-for="(item, index) in userList" :key="index">
+				<span
+					:class="[item.active ? 'acitve' : '', 'nickName']"
+					@click="changeUser(item)"
+					>{{ item.nickName }}</span
+				>
+			</div>
+		</div>
+
+		<h2>{{ connected ? "连接" : "断开" }} {{ roomId }}</h2>
+		<h2>{{ errorMsg }}</h2>
 		<div>
 			<div v-for="(item, index) in msgList" :key="index">
 				<span>{{ item.sendUserName }} 说: </span>
@@ -11,7 +22,7 @@
 		<div>
 			<el-form @submit="send">
 				<el-form-item>
-					<el-input v-model="msg" />
+					<el-input :disabled="!!!roomId" v-model="msg" />
 					<el-button @click="send"> 发送</el-button>
 				</el-form-item>
 			</el-form>
@@ -24,7 +35,7 @@ import { useCrud, useTable, useUpsert } from "@cool-vue/crud";
 import { useCool } from "/@/cool";
 import { ElMessage, ElMessageBox } from "element-plus";
 
-import { ref } from "vue";
+import { computed, onUnmounted, ref } from "vue";
 import io from "socket.io-client";
 import { uuid } from "/@/cool/utils";
 import { useBase } from "/$/base";
@@ -33,12 +44,28 @@ const { user } = useBase();
 let msgList = ref<any[]>([]);
 let msg = ref<string>("");
 let roomId = ref<string>("");
+const connected = ref<boolean>(false);
+const userList = ref<any[]>([]);
+const errorMsg = ref("");
 
 console.log("user.info: ", user.info);
+
+// 是否是客服
+const isService = computed(() => {
+	return user.info?.id === 1;
+});
 
 const socket = io(
 	`http://localhost:7001/chat?userId=${user.info?.id}&type=${user.info?.id === 1 ? "service" : "user"}`
 );
+
+socket.on("connect", () => {
+	connected.value = true;
+});
+
+socket.on("disconnect", () => {
+	connected.value = false;
+});
 
 socket.on("data", (msg) => {
 	console.log("服务端消息", msg);
@@ -56,13 +83,69 @@ socket.on("data", (msg) => {
 			break;
 	}
 });
+// 连接成功
+socket.on("connect-socket", (msg: any) => {
+	console.log("connect-msg: ", msg);
+	// roomId.value = msg.roomId;
+	if (!isService.value) {
+		roomId.value = msg.roomId;
+	}
 
-function send(e) {
-	socket.emit("sendMsg", { userId: user.info?.id, msg: msg.value });
+	ElMessage.success("连接成功");
+});
+
+// 有用户加入连接
+socket.on("connect-user-add", (data) => {
+	console.log("connect-user-add-msg: ", data);
+	userList.value = data.data.map((item) => {
+		return {
+			...item,
+			active: false
+		};
+	});
+	ElMessage.success(data.message);
+});
+
+// 消息监听
+socket.on("message", (msg: any) => {
+	console.log("message-msg: ", msg);
+
+	msgList.value.push(msg);
+});
+
+// 没有客服的等待
+socket.on("no-service", (msg: any) => {
+	console.log("wait-msg: ", msg);
+	errorMsg.value = msg.message;
+});
+
+// 用户断开连接
+socket.on("disconnect-user", (data: any) => {
+	console.log("disconnect-user-msg: ", data);
+	userList.value = data.data.map((item) => {
+		return {
+			...item,
+			active: false
+		};
+	});
+});
+
+function send(e: { preventDefault: () => void }) {
+	socket.emit("sendMsg", { userId: user.info?.id, msg: msg.value, roomId: roomId.value });
 	// msgList.value.push({ id: "我", msg: msg.value });
 	msg.value = "";
 	e.preventDefault();
 	return false;
+}
+
+function changeUser(item) {
+	userList.value.forEach((item) => {
+		item.active = false;
+	});
+
+	item.active = true;
+
+	roomId.value = `room-${item.id}-${user.info?.id}`;
 }
 
 const { service } = useCool();
@@ -93,4 +176,18 @@ function refresh(params?: any) {
 }
 
 const urls = ref<string[]>([]);
+
+// 页面销毁断开socket
+onUnmounted(() => {
+	socket.disconnect();
+});
 </script>
+<style lang="scss" scoped>
+.nickName {
+	cursor: pointer;
+}
+
+.acitve {
+	color: red;
+}
+</style>
